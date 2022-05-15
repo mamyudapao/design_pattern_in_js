b1e14610853dbdca877a6ccfbf881437dfee3d48
kenichi noshian <nosikenit@gmail.com>
2022-05-15 17:07:04 +0900

    proxy

---
+++ proxy/index.js
@@ -0,0 +1,17 @@
+const person = {
+    name: "John Doe",
+    age: 42,
+    nationality: "American",
+};
+
+const personProxy = new Proxy(person, {
+    get: (obj, prop) => {
+        console.log(`The value of ${prop} is ${Reflect.get(obj, prop)}`);
+    },
+    set: (obj, prop, value) => {
+        console.log(`Changed ${prop} from ${obj[prop]} to ${value}`);
+        Reflect.set(obj, prop, value);
+    },
+});
+personProxy.age;
+personProxy.nationality = "Italian";
